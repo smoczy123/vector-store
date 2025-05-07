c3be5e89aa8375c1a680c06c81728b90201d4ad0
Michał Hudobski <michal.hudobski@scylladb.com>
2025-05-07 11:56:43 +0200

    Implement Index:Ann for Opensearch

--- src/index/opensearch.rs
+++ src/index/opensearch.rs
@@ -8,14 +8,17 @@
 
 use crate::Connectivity;
 use crate::Dimensions;
+use crate::Distance;
 use crate::Embedding;
 use crate::ExpansionAdd;
 use crate::ExpansionSearch;
 use crate::IndexFactory;
 use crate::IndexId;
+use crate::Limit;
 use crate::PrimaryKey;
 use crate::SpaceType;
 use crate::index::actor::Index;
+use anyhow::anyhow;
 use bimap::BiMap;
 use opensearch::DeleteParts;
 use opensearch::IndexParts;
@@ -24,19 +27,25 @@ use opensearch::http::Url;
 use opensearch::http::transport::SingleNodeConnectionPool;
 use opensearch::http::transport::TransportBuilder;
 use opensearch::indices::IndicesCreateParts;
+use serde_json::Value;
 use serde_json::json;
 use std::fmt::Display;
+use std::num::NonZeroUsize;
 use std::sync::Arc;
 use std::sync::RwLock;
 use std::sync::atomic::AtomicU64;
 use std::sync::atomic::Ordering;
 use tokio::sync::Semaphore;
 use tokio::sync::mpsc;
+use tokio::sync::oneshot;
 use tracing::Instrument;
 use tracing::debug;
 use tracing::debug_span;
 use tracing::error;
 use tracing::info;
+use tracing::trace;
+
+use super::actor::AnnR;
 
 pub struct OpenSearchIndexFactory {
     client: Arc<OpenSearch>,
@@ -236,7 +245,7 @@ pub fn new(
 
 async fn process(
     msg: Index,
-    _dimensions: Dimensions,
+    dimensions: Dimensions,
     id: Arc<IndexId>,
     keys: Arc<RwLock<BiMap<PrimaryKey, Key>>>,
     opensearch_key: Arc<AtomicU64>,
@@ -253,11 +262,7 @@ async fn process(
             embedding,
             limit,
             tx,
-        } => {
-            let _ = embedding;
-            let _ = limit;
-            let _ = tx;
-        }
+        } => ann(id, tx, keys, embedding, dimensions, limit, client).await,
         Index::Count { tx } => {
             let _ = tx;
         }
@@ -350,4 +355,95 @@ async fn remove(
     if response.is_err() {
         keys.write().unwrap().insert(primary_key, key);
     }
+}
+
+async fn ann(
+    id: Arc<IndexId>,
+    tx_ann: oneshot::Sender<AnnR>,
+    keys: Arc<RwLock<BiMap<PrimaryKey, Key>>>,
+    embedding: Embedding,
+    dimensions: Dimensions,
+    limit: Limit,
+    client: Arc<OpenSearch>,
+) {
+    let Some(embedding_len) = NonZeroUsize::new(embedding.0.len()) else {
+        tx_ann
+            .send(Err(anyhow!("ann: embedding dimensions == 0")))
+            .unwrap_or_else(|_| trace!("ann: unable to send error response (zero dimensions)"));
+        return;
+    };
+    if embedding_len != dimensions.0 {
+        tx_ann
+            .send(Err(anyhow!(
+                "ann: wrong embedding dimensions: {embedding_len} != {dimensions}",
+            )))
+            .unwrap_or_else(|_| trace!("ann: unable to send error response (wrong dimensions)"));
+        return;
+    }
+
+    let response = client
+        .search(opensearch::SearchParts::Index(&[&id.0]))
+        .body(json!({
+            "query": {
+                "knn": {
+                    "vector": {
+                        "vector": embedding.0,
+                        "k": limit.0,
+                    }
+                }
+            }
+        }))
+        .send()
+        .await
+        .map_or_else(
+            Err,
+            opensearch::http::response::Response::error_for_status_code,
+        )
+        .map_err(|err| {
+            error!("ann: unable to search for embedding: {err}");
+        });
+
+    if response.is_err() {
+        _ = tx_ann.send(Err(anyhow!("ann: unable to search for embedding")));
+        return;
+    }
+
+    let response_body = response.unwrap().json::<Value>().await;
+
+    if response_body.is_err() {
+        _ = tx_ann.send(Err(anyhow!("ann: unable to search for embedding")));
+        return;
+    }
+    let response_body = response_body.unwrap();
+
+    let hits = response_body
+        .get("hits")
+        .and_then(|hits| hits.get("hits"))
+        .and_then(|hits| hits.as_array());
+
+    if hits.is_none() {
+        _ = tx_ann.send(Err(anyhow!("ann: unable to search for embedding")));
+        return;
+    }
+    let hits = hits
+        .unwrap()
+        .iter()
+        .map(|hit| {
+            let id = hit["_id"].as_str().unwrap();
+            let score = hit["_score"].as_f64().unwrap();
+            let keys = keys.read().unwrap();
+            let key = keys.get_by_right(&Key(id.parse::<u64>().unwrap())).unwrap();
+            (key.clone(), score)
+        })
+        .collect::<Vec<_>>();
+
+    let (keys, scores): (Vec<_>, Vec<_>) = hits.iter().cloned().unzip();
+    let distances = scores
+        .iter()
+        .map(|score| Distance(*score as f32))
+        .collect::<Vec<_>>();
+
+    tx_ann
+        .send(Ok((keys, distances)))
+        .unwrap_or_else(|_| trace!("ann: unable to send response"));
 }
