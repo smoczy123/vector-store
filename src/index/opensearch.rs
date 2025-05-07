e36f2b778ba56d9dfc5a56fd473d2a07345047a3
Michał Hudobski <michal.hudobski@scylladb.com>
2025-05-07 11:59:22 +0200

    Implement Index:Count for Opensearch

--- src/index/opensearch.rs
+++ src/index/opensearch.rs
@@ -466,3 +466,44 @@ async fn ann(
         )
         .unwrap_or_else(|_| trace!("ann: unable to send response"));
 }
+
+async fn count(
+    id: Arc<IndexId>,
+    tx: oneshot::Sender<usize>,
+    client: Arc<OpenSearch>,
+) {
+    let response = client
+        .count(opensearch::CountParts::Index(&[&id.0]))
+        .send()
+        .await
+        .map_or_else(
+            Err,
+            opensearch::http::response::Response::error_for_status_code,
+        )
+        .map_err(|err| {
+            error!("count: unable to count embeddings: {err}");
+        });
+
+    if response.is_err() {
+        _ = tx.send(0);
+        return;
+    }
+
+    let response_body = response.unwrap().json::<Value>().await;
+
+    if response_body.is_err() {
+        _ = tx.send(0);
+        return;
+    }
+    let response_body = response_body.unwrap();
+
+    let count = response_body["count"].as_u64();
+
+    if count.is_none() {
+        _ = tx.send(0);
+        return;
+    }
+    let count = count.unwrap();
+
+    _ = tx.send(count as usize);
+}
