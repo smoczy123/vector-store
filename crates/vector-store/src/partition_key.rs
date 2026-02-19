417bbd26440819ff99d94ca8c85a96e2988a3eb6
Pawel Pery <pawel.pery@scylladb.com>
2026-02-19 20:19:01 +0100

    vector-store: implement PartitionKey
    In the following commits we will introduce new data model, which will use
    PartitionKey as a type for PartitionKey of the index. This commit implements
    this type similar to PrimaryKey.

---
+++ crates/vector-store/src/partition_key.rs
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2026-present ScyllaDB
+ * SPDX-License-Identifier: LicenseRef-ScyllaDB-Source-Available-1.0
+ */
+
+use crate::invariant_key::InvariantKey;
+use scylla::value::CqlValue;
+
+/// This is a thin newtype around [`InvariantKey`] providing partition-key-specific
+/// semantics.
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct PartitionKey(InvariantKey);
+
+impl PartitionKey {
+    pub fn len(&self) -> usize {
+        self.0.len()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.0.is_empty()
+    }
+
+    pub fn get(&self, idx: usize) -> Option<CqlValue> {
+        self.0.get(idx)
+    }
+}
+
+impl FromIterator<CqlValue> for PartitionKey {
+    fn from_iter<I: IntoIterator<Item = CqlValue>>(iter: I) -> Self {
+        Self(InvariantKey::from_iter(iter))
+    }
+}
+
+impl<I: IntoIterator<Item = CqlValue>> From<I> for PartitionKey {
+    fn from(iter: I) -> Self {
+        Self(InvariantKey::from_iter(iter))
+    }
+}
