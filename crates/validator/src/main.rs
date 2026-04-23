d4a85f2a3b6c321183f4d65b99aa6a8afe037a14
Pawel Pery <pawel.pery@scylladb.com>
2026-04-23 17:01:10 +0200

    validator: prepare running validator tests from reorganized crates
    The commit finishes move validator-vector-store into validator crate.  It
    refactors validator-engine to retrieve async fn for registering - and now
    validator-engine doesn't depend on validator anymore.
    
    Fixes: VECTOR-654

--- crates/validator/src/main.rs
+++ crates/validator/src/main.rs
@@ -3,6 +3,15 @@
  * SPDX-License-Identifier: LicenseRef-ScyllaDB-Source-Available-1.0
  */
 
+use async_backtrace::framed;
+use vector_search_validator_tests::TestCase;
+
+#[framed]
+/// Returns a vector of all known test cases to be run. Each test case is registered with a name
+async fn register() -> Vec<(String, TestCase)> {
+    vector_search_validator::test_cases().await.collect()
+}
+
 fn main() -> Result<(), &'static str> {
-    vector_search_validator_engine::run()
+    vector_search_validator_engine::run(register)
 }
